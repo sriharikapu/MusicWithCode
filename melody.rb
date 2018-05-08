3fc9d99a240cfecc29259e4230a244a7d0fdb1ef
sriharikapu <sriharikapu@mail.com>
2018-05-08 22:16:45 +0800

    :100:
    error in shuffle method

--- melody.rb
+++ melody.rb
@@ -1,7 +1,7 @@
 live_loop :random_riff do
  use_synth :dsaw
  use_random_seed 3
- notes = (scale :e3, :minor_pentatonic).shuffe
+ notes = (scale :e3, :minor_pentatonic).shuffle
  play notes.tick, release: 0.25, cutoff: 80
  sleep 0.25
 end
