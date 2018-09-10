8ad322b7accd2e1377b5f0f0f63607f5247f84f9
sriharikapu <srihari@blockchaintmhub.io>
2018-09-10 15:12:52 +0800

    Update tron.rb

--- tron.rb
+++ tron.rb
@@ -4,7 +4,7 @@ loop do
       with_fx(:reverb, room: 0.5, mix: 0.3) do
         start_note = chord([:b1, :b2, :e1, :e2, :b3, :e3].choose, :minor).choose
         final_note = chord([:b1, :b2, :e1, :e2, :b3, :e3].choose, :minor).choose
-
+        sleep 1
         p = play start_note, release: 8, note_slide: 4, cutoff: 30, cutoff_slide: 4, detune: rrand(0, 0.2), pan: rrand(-1, 0), pan_slide: rrand(4, 8)
         control p, note: final_note, cutoff: rrand(80, 120), pan: rrand(0, 1)
       end
